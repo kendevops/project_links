82101f758390b867c2887e97361774db8c4ecf49
Kenneth Aladi <kennethmichael11@gmail.com>
2019-10-01 19:00:29 +0100

    Modified the Logo and Statics

--- components/Logo.js
+++ components/Logo.js
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
 const Logo = () => (
-    <div className="Logo">
-        <Link href="/">
-            <a>KEN</a>
-        </Link>
-    </div>
+  <div className="Logo">
+    <Link href="/">
+      <a>
+        <img src="/static/android-chrome-192x192.png" />
+      </a>
+    </Link>
+  </div>
 );
-
-
 
 export default Logo;
